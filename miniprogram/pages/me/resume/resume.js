73c4d81736931cbef3cb8b00f177ba204790d209
guoming chen <chenguoming2015@163.com>
2020-07-24 21:03:53 +0800

    微信小程序个人简历

---
+++ miniprogram/pages/me/resume/resume.js
@@ -0,0 +1,119 @@
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+    show:{
+      basicInfo:true,
+      professionalSkill:false,
+      educationExperience:true,
+      workExperience:true,
+      projectExperience:false,
+      USOP:false,
+      DBMS:false,
+      UFE:false,
+      selfEvaluation:true,
+    },
+    mappingToCh:{
+      basicInfo:'基本信息',
+      name:'姓名',
+      target:'求职意向',
+      birthday:'出生年月',
+      phone:'手机',
+      education:'学历',
+      major:'专业',
+      experience:'工作经验',
+      email:'邮箱',
+      educationExperience:'教育背景',
+      workExperience:'工作经历',
+      professionalSkill:'专业技能',
+      projectExperience: '项目经历',
+      selfEvaluation:'自我评价',
+      description:'项目描述',
+      responsibilities:'主要职责',
+      performance:'工作业绩'
+    },
+    basicInfo:{
+      name:'陈国明',
+      target:'前端开发工程师',
+      birthday:'1990.03',
+      phone:'18822253630',
+      education:'硕士',
+      major:'计算机科学与技术',
+      experience:'四年',
+      email:' chenguoming2015@163.com',
+      image:'/images/name.png'
+    },
+    educationExperience:[
+      {duration:'2009.09——2013.06',school:'河北科技大学',level:'本科',major:'数学与应用数学'},
+      {duration:'2013.09——2016.06',school:'中国民航大学',level:'硕士',major:'计算机科学与技术'}
+    ],
+    workExperience:[
+      {duration:'2016.07—— 至今 ', company:'神州租车（天津）有限公司',position:'运维开发工程师'}
+    ],
+    professionalSkill:[
+      '熟练掌握web前端HTML/CSS/JavaScript/jQuery等前端技术',
+      '熟练使用VUE+elementUI+vuex框架，掌握其生命周期、渲染机制等原理',
+      '熟悉ES6语法，熟悉webpack打包工具',
+      '熟悉postman、git、nginx等日常工具',
+      '了解微信小程序、移动端h5开发',
+      '了解go语言，使用过iris框架',
+      '了解python和shell基本命令',
+    ],
+    projectExperience:[
+      {
+        name:'自助运维平台',
+        shortName:'USOP',
+        description:'主要包括服务树，发布系统，监控系统，申请系统，日志系统，权限系统，配置管理等运维子系统。平台建设从专注于运维管理逐步到扩展到整个公司的自助运维；技术上也不断地工程化、标准化，目前平台登录用户1800+。',
+        responsibilities:[
+          '主导了权限系统、配置管理系统、监控系统的原型设计沟通及前端开发;',
+          '协助完成服务树、发布系统、申请系统发布的前端开发工作；参与了用户空间及新版权限系统的部分开发工作;',
+          '技术方面推进平台2.0版向vue转型，解决过渡期间项目兼容性问题并封装常用公共组件，如表格、树、图等；和后端一起定位并解决部分接口慢的问题。'
+        ],
+        performance:[
+          '平台大大提升了运维工作效率，解决了运维日常管理的痛点，向平台化迈进了一步；方便了开发的自助查看、发布、申请服务等的需要；',
+          '技术上从最初的jquery+模板的方式逐步迁移到了VUE（elementUI组件)，整个平台更加工程化、规范化，极大的提高了开发维护效率。'
+        ]
+      },
+      {
+        name:'数据库管理平台',
+        shortName:'DBMS',
+        description:'主要包括资产管理，审核发布，数据检索，调度系统，数据同步，数据库监控等子系统；结合USOP平台的申请系统形成以数据库为中心，实现不同角色各种操作的闭环。让DBA的日常操作从执行各种脚本中过渡到操作统一的管理平台。',
+        responsibilities:[
+          '主导资产管理，审核发布，数据检索功能开发；并根据需求变动，持续迭代优化审核发布和数据检索；',
+          '参与项目初始选型时，用smartadmin模板（angular）取代jquery，提高前端开发效率；部门转型VUE后，协助dbms向vue过渡转化；',
+          '数据检索操作简洁性、易用性的改造；增加收藏、自动填充、快捷操作等提高用户使用效率。'
+        ],
+        performance:[
+          '审核发布平台方便了值班DBA的日常工作，大大减少了人为操作失误，同时加快了审批速度；辅助DBA推广了公司SQL书写规范及线上数据库变更流程；',
+          '数据检索结合权限和敏感字段管理，灵活方便的满足了开发人员的查询痛点，目前有收藏记录的数据检索用户800+。'
+        ]
+      },
+      {
+        name:'流量管理平台',
+        shortName:'UFE',
+        description:'主要包括服务指标度量，七层和四层负载均衡系统、WAF防护系统、缓存系统、名字服务等；是公司层面服务流量的总入口。',
+        responsibilities:[
+          '负责UFE平管理端的整体原型设计和前端开发工作；',
+          '推进UFE2.0改版，为提高开发效率，图表以嵌套grafana为主；新功能也逐步转向vue框架。'
+        ],
+        performance:[
+          '平台支持管理接入节点，配置变更生效；流量状态的监控检查以及日志聚合查询，满足了运维对服务监控管理需求，平台已经稳定运行三年；',
+          '平台提供多维度的接入日志查询和服务指标监控，方便开发查询和定位问题。'
+        ]
+      }
+    ],
+    selfEvaluation:[
+      '为人诚恳，讲诚信',
+      '工作积极负责，善始善终',
+      '乐于学习、钻研新技术新框架'
+    ]
+  },
+  onLoad: function() {
+  
+  },
+  toggle: function(event) {
+    var item = event.currentTarget.dataset.show;
+    this.setData({ ['show.'+item]: !this.data.show[item] })
+  }
+})
--- miniprogram/pages/wx/index.js
+++ miniprogram/pages/wx/index.js
@@ -3,118 +3,33 @@ const app = getApp()
 
 Page({
   data: {
-    avatarUrl: './user-unlogin.png',
-    userInfo: {},
-    logged: false,
-    takeSession: false,
-    requestResult: ''
+    msgNumber:17,
+    list:[
+      {imagurl:'/images/wechatHL.png',name:'刘桐风',date:'13:53',info:'等疫苗吧'},
+      {imagurl:'/images/wechatHL.png',name:'枫丹白露',date:'星期三',info:' 501498361'},
+      {imagurl:'/images/wechat.png',name:'袁丽娜',date:'星期一',info:'嗯。'},
+      {imagurl:'/images/wechatHL.png',name:'一家子',date:'星期一',info:'[图片]'},
+      {imagurl:'/images/wechat.png',name:'小站技术交流 01 群',date:'14:12',info:'[57条]爽shi(David)：[衰]'},
+      {imagurl:'/images/wechatHL.png',name:'订阅好消息',date:'13:46',info:'[23条]AI前线：0.5元一份人脸信息!业界...'},
+    ]
   },
-
   onLoad: function() {
-    if (!wx.cloud) {
-      wx.redirectTo({
-        url: '../chooseLib/chooseLib',
-      })
-      return
-    }
-
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
-              })
-            }
-          })
-        }
-      }
-    })
-  },
-
-  onGetUserInfo: function(e) {
-    if (!this.data.logged && e.detail.userInfo) {
-      this.setData({
-        logged: true,
-        avatarUrl: e.detail.userInfo.avatarUrl,
-        userInfo: e.detail.userInfo
-      })
-    }
-  },
-
-  onGetOpenid: function() {
-    // 调用云函数
-    wx.cloud.callFunction({
-      name: 'login',
-      data: {},
-      success: res => {
-        console.log('[云函数] [login] user openid: ', res.result.openid)
-        app.globalData.openid = res.result.openid
-        wx.navigateTo({
-          url: '../userConsole/userConsole',
-        })
+    var title = '微信'+'('+this.data.msgNumber+')'
+    wx.setNavigationBarTitle({
+      title: title,
+      complete: (res) => {},
+      fail: (res) => {},
+      success: (res) => {
       },
-      fail: err => {
-        console.error('[云函数] [login] 调用失败', err)
-        wx.navigateTo({
-          url: '../deployFunctions/deployFunctions',
-        })
-      }
     })
-  },
 
-  // 上传图片
-  doUpload: function () {
-    // 选择图片
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['compressed'],
-      sourceType: ['album', 'camera'],
-      success: function (res) {
-
-        wx.showLoading({
-          title: '上传中',
-        })
-
-        const filePath = res.tempFilePaths[0]
-        
-        // 上传图片
-        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
-        wx.cloud.uploadFile({
-          cloudPath,
-          filePath,
-          success: res => {
-            console.log('[上传文件] 成功：', res)
-
-            app.globalData.fileID = res.fileID
-            app.globalData.cloudPath = cloudPath
-            app.globalData.imagePath = filePath
-            
-            wx.navigateTo({
-              url: '../storageConsole/storageConsole'
-            })
-          },
-          fail: e => {
-            console.error('[上传文件] 失败：', e)
-            wx.showToast({
-              icon: 'none',
-              title: '上传失败',
-            })
-          },
-          complete: () => {
-            wx.hideLoading()
-          }
-        })
-
-      },
-      fail: e => {
-        console.error(e)
-      }
+    wx.showTabBarRedDot({
+      index: 2,
     })
-  },
 
+    wx.setTabBarBadge({
+      index: 0,
+      text: this.data.msgNumber+'',
+    })
+  }
 })
